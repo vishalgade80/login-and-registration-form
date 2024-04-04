10c37809420b883058438cb2520088f7ba842f94
VISHAL GADE <126267675+vishalgade80@users.noreply.github.com>
2024-04-04 07:05:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import dotenv from 'dotenv';
 import bodyParser from 'body-parser'
 import  Connection from './Database/Connection.js';
 import router from './Router/Router.js';
@@ -8,9 +7,8 @@ import cors from 'cors';
 
 Connection();
 const app = express();
-dotenv.config();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(router)
-const Port = process.env.PORT
+const Port = 4999
 app.listen(Port,()=>{console.log(`Server is running on ${Port}`)})
